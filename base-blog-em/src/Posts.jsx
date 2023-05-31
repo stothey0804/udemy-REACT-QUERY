74a2202db765e1db3ad000d7a76153abd71aec52
Seyoung Kim <stothey0804@gmail.com>
2023-05-31 17:25:20 +0900

    post pagination prefetching

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,12 +15,26 @@ export function Posts() {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [selectedPost, setSelectedPost] = useState(null);
 
+	const queryClient = useQueryClient();
+
+	// 다음페이지의 데이터를 미리 가져온다.
+	useEffect(() => {
+		if(currentPage < maxPostPage) {
+			const nextPage = currentPage + 1;
+			queryClient.prefetchQuery(
+				['posts', nextPage],
+				() => fetchPosts(nextPage)
+			);
+		}
+	}, [currentPage, queryClient]);
+
 	// replace with useQuery
 	const { data, isLoading, isError } = useQuery(
 		["posts", currentPage], 
 		() => fetchPosts(currentPage), 
 		{
 			staleTime: 2000,
+			keepPreviousData: true,
 		},	
 	);
 
