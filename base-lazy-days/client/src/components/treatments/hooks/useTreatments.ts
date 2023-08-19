866c00ce334b7151011dc9e22b79b3283a8427dc
stothey0804 <stothey0804@gmail.com>
2023-08-19 19:25:49 +0900

    day spa app > useIsFecting, error handler 추가

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,6 +11,7 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const { data } = useQuery(queryKeys.treatments, getTreatments);
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
